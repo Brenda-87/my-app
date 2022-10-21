890f9db958f1e6f84faf0009699d32fa977a8ccc
Brenda <brenda@Rolands-MacBook-Pro.local>
2022-10-21 12:01:06 +0200

    Added interactive functionality to input and button. Also linked Bootstrap.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-app';
+  text = '';
+  reversedText = '';
+
+  onClickReverse() {
+    this.reversedText = this.text.split('').reverse().join('');
+  }
+
+  onInputText($event: any) {
+    this.text = $event.target.value;
+  }
 }
